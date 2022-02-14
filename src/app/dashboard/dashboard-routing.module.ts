f607db98f16ffadd8fe1798a10f1072731c0b4d6
Alibek Sultanov <asultanov@dssinc.com>
2022-02-14 15:57:49 -0500

    added create client and create admin componenets with routing.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CreateAdminComponent } from './create-admin/create-admin.component';
+import { CreateClientComponent } from './create-client/create-client.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminDashboardComponent,
+    children: [
+      {
+        path:'create-client',
+        component: CreateClientComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'create-admin',
+        component: CreateAdminComponent,
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CreateClientComponent } from './create-client/create-client.component';
+import { CreateAdminComponent } from './create-admin/create-admin.component';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    CreateClientComponent,
+    CreateAdminComponent
   ],
   imports: [
     CommonModule,
